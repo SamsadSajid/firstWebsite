d65fe4985ba59045dbc67c2965ef7d9563d797c5
shamsad <1405118.ssr@ugrad.cse.buet.ac.bd>
2017-02-19 19:45:08 +0600

    added user registration

--- music/views.py
+++ music/views.py
@@ -1,5 +1,9 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import Userform
 from music.models import Album
 
 
@@ -19,3 +23,36 @@ class DetailView(generic.DetailView):
 class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'logo']
+
+
+class  UserFormView(View):
+    form_class = Userform
+    template_name = 'music/registration.html'
+    # for new user
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form, })
+    # process form for data
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user.set_password(password)
+            user.save()
+
+            #
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form, })
